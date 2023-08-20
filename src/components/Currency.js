ca99cd4462b429eb34930dd302582db1b518c49a
sitangshu chand <sitangshu.chand@outlook.com>
2023-08-20 14:34:02 -0400

    added select item

--- src/components/Currency.js
+++ src/components/Currency.js
@@ -1,27 +1,51 @@
 import { useState } from "react";
+import Select, { components } from "react-select";
+import './Currency.css'
 
 const Currency=()=>{
-    const [currency,setCurrency]=useState('$ Dollar');
+    const [currency,setCurrency]=useState({ label: "Currency (£ Pound)", value: "$", color: "#badbcc" });
 
     const changeHandler=(event)=>{
-        setCurrency(event.target.value)
+        console.log('inside change handler');
+        console.log(`Currency (${event.label})`)
+        console.log(event);
+        setCurrency({...currency,value:event.value,label: `Currency (${event.label})`})
 
     }
+    const options = [
+        { label: "$ Dollar", value: "$", color: "#badbcc" },
+        { label: "£ Pound", value: "£", color: "#badbcc" },
+        { label: "€ Euro", value: "€", color: "#badbcc" },
+        { label: "₹ Rupee", value: "₹", color: "#badbcc" }
+      ];
+    const Option = props => {
+        return (
+          <div style={{ backgroundColor: props.data.color }}>
+            <components.Option {...props} >
+                
+                </components.Option>
+          </div>
+        );
+      };
+
+      const style = {
+        control: (base) => ({
+            
+          ...base,
+          border: 0,
+          // This line disable the blue border
+          boxShadow: "none",
+          backgroundColor:"#badbcc",  
+          
+        //   borderColor: state.isSelected ? 'purple' : 'red'         
+        })
+      };
  return (
      <>
-    <div className="alert alert-success">
-        <label htmlFor="inputGroupSelect01" >Currency (
-            <select id="inputGroupSelect01" onChange={changeHandler}>
-                <option defaultValue>{currency}</option>
-                <option value="$ Dollar" name="dollar"> $ Dollar</option>
-                <option value="£ Pound" name="pound">£ Pound</option>
-                <option value="€ Euro" name="euro">€ Euro</option>
-                <option value="₹ Rupee" name="Rupee">₹ Rupee</option>
-
-            </select>
-        )
-            </label>
-</div>
+     <div >
+      <Select styles={style} defaultValue={currency} components={{Option}} options={options}  onChange={changeHandler} />
+    </div>
+    
 </>
  )
 }
